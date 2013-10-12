1cb6f2d534a5f5554b95da8be69386ea834b82b9
Johan Baversjo <baversjo@gmail.com>
2013-10-12 02:26:10 -0700

    documented all steps for api call

--- web.js
+++ web.js
@@ -1,14 +1,54 @@
 var express = require("express");
 var app = express();
 
+app.use(express.bodyParser());
+
 /* serves main page */
 app.get("/", function(req, res) {
 	res.sendfile('index.html');
 });
 
-app.post("/howhipsteris", function(req, res) { 
-/* some server side logic */
-res.send("Error: to be implemented");
+app.get("/howhipsteris", function(req, res) { 
+	var fb_id = req.query.fb_id,
+	access_token = req.query.access_token;
+
+	var resp = "fb_id:" + fb_id + " access_token:" + access_token;
+
+	if(fb_id && access_token){
+		resp += " request is OK!";
+		res.status(200);
+	}else{
+		resp += " bad request, missing param!";
+		res.status(400);
+	}
+
+
+
+	//step one:
+	//get list of friend's fb_ids:
+	//input: fb_id, access_token
+	//callback: array with fb_ids
+	//friends_for(fb_id,access_token,callback);
+
+	//step two:
+	//find user's all band likes
+	//input: one fb_id
+	//callback: array elements: {band: "Madeon", like_date: 1235125213}
+	//find_band_likes(fb_id,callback)
+
+	//step three:
+	//add each band's wikipedia creation date to hash above (step three will be done per-user)
+	//input: array elements: {band: "Madeon", like_date: 1235125213}
+	//callback: array elements: {band: "Madeon", like_date: 1235125213, wiki_date: 12784122}
+
+	//step four:
+	//calculate a hipster score for a user
+	//input: array elements: {band: "Madeon", like_date: 1235125213, wiki_date: 12784122}
+	//callback: a float between 0.0-100.0
+
+
+	//step five: just call the pusher api.
+	res.end(resp);
 });
 
 var port = process.env.PORT || 5000;
